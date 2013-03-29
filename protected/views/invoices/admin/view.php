1c1e9b61396996dddf2b5f082578b7527567df99
Jakub Wawrzyniak <jakub@itpeople.pl>
2013-03-29 11:58:37 +0100

    admin view fix

--- protected/views/invoices/admin/view.php
+++ protected/views/invoices/admin/view.php
@@ -28,7 +28,7 @@ $this->menu=array(
 	<div class="control-group">
 		<label class="control-label" for="Invoices_status">Aktualny skan faktury</label>
 		<div class="controls" style="line-height: 2em;">
-		<?php if(!is_null($model->file_src)) echo '<a href="'.Yii::app()->request->baseUrl.'/invs/'.$unit->id.'/'.$model->object_id.'/'.$model->file_src.'" title="podgląd dokumentu"><img src="'.Yii::app()->request->baseUrl.'/images/pdf.png" alt="podgląd dokumentu" /></a>'; else echo 'Brak powiązanego pliku.' ?>
+		<?php if(!is_null($model->file_src)) echo '<a href="'.Yii::app()->request->baseUrl.'/invs/'.$unit->unit_id.'/'.$model->object_id.'/'.$model->file_src.'" title="podgląd dokumentu"><img src="'.Yii::app()->request->baseUrl.'/images/pdf.png" alt="podgląd dokumentu" /></a>'; else echo 'Brak powiązanego pliku.' ?>
 		</div>
 	</div>
 </fieldset>
